cd4206e73b09cbd6f480182c906e87af025c7977
Sean Thweatt <seant21@vt.edu>
2013-12-10 20:45:04 -0500

    Maybe this will work

--- src/org/edu/vt/pingpongapp/InGameActivity.java
+++ src/org/edu/vt/pingpongapp/InGameActivity.java
@@ -67,7 +67,6 @@ public class InGameActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.log_in, menu);
 		return true;
 	}
